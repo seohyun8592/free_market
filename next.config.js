2d3146d06ecf73500ceab91401865f7a5832bba5
lanie <lanie.seo@kakaocorp.com>
2024-04-30 10:56:59 +0900

    test8

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/test`,
       },
     ]
   },
