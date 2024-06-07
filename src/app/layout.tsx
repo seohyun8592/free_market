d79b982f08a4358fbb63fd41ac99a7dd4f1a3ba9
lanie <lanie.seo@kakaocorp.com>
2024-06-07 13:39:02 +0900

    로그인 페이지명 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 
-// import { Inter } from "next/font/google";
 import Header from "@/components/layout/header"
-// const inter = Inter({ subsets: ["latin"] });
 import type { Metadata } from "next"
 
 import "../../public/assets/styles/style.scss"
@@ -12,11 +10,6 @@ export const metadata: Metadata = {
   description:
     "집에 필요없는물건, 돈주고 팔기 어려운 물건, 당장 급하게 처분해야하는 물건 싹 다 나눠버리세요!",
 }
-
-// export const metadata: Metadata = {
-//   title: { default: "서현 블로그", template: "서현 블로그 | %s" },
-//   description: "프론트엔드 개발자 블로그",
-// };
 
 export default function RootLayout({
   children,
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import LoginForm from "@/components/layout/LoginForm"
 
-export default function Page() {
+export default function LoginPage() {
   return (
     <section className="contents__wrap login">
       <LoginForm />
