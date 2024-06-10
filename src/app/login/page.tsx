c8a24df0792c5b3ec6a27f3bd7dee568ef625363
김서현 <shhs0902@naver.com>
2024-06-10 22:28:37 +0900

    test333

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,27 @@
-// import React from "react"
-// import LoginForm from "@/components/layout/LoginForm"
-// export default function LoginPage() {
-//   return (
-//     <section className="contents__wrap login">
-//       <LoginForm />
-//     </section>
-//   )
-// }
-import React, { Suspense, lazy } from "react"
+import React from "react";
 
-const LoginForm = lazy(() => import("@/components/layout/LoginForm"))
+import dynamic from 'next/dynamic';
 
 export default function LoginPage() {
+  const LoginForm = dynamic(() => import("@/components/layout/LoginForm"), {
+    ssr: false, // 이 설정이 CSR 컴포넌트로 만듭니다.
+  })
   return (
     <section className="contents__wrap login">
-      <Suspense fallback={<div>Loading...</div>}>
-        <LoginForm />
-      </Suspense>
+      <LoginForm />
     </section>
   )
 }
+// import React, { Suspense, lazy } from "react"
+
+// const LoginForm = lazy(() => import("@/components/layout/LoginForm"))
+
+// export default function LoginPage() {
+//   return (
+//     <section className="contents__wrap login">
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <LoginForm />
+//       </Suspense>
+//     </section>
+//   )
+// }
