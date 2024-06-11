5c32423a9443274fa725855210e0b71b435159e0
lanie <lanie.seo@kakaocorp.com>
2024-06-11 09:54:42 +0900

    test444

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,10 @@
-// import React from "react"
-// import dynamic from "next/dynamic"
-// export default function LoginPage() {
-//   const LoginForm = dynamic(() => import("@/components/layout/LoginForm"), {
-//     ssr: false, // 이 설정이 CSR 컴포넌트로 만듭니다.
-//   })
-//   return (
-//     <section className="contents__wrap login">
-//       <LoginForm />
-//     </section>
-//   )
-// }
 import React from "react"
 
-import LoginForm from "@/components/layout/LoginForm"
+import dynamic from "next/dynamic"
 
+const LoginForm = dynamic(() => import("@/components/layout/LoginForm"), {
+  ssr: false, // 이 설정이 CSR 컴포넌트로 만듭니다.
+})
 export default function LoginPage() {
   return (
     <section className="contents__wrap login">
@@ -21,3 +12,14 @@ export default function LoginPage() {
     </section>
   )
 }
+// import React from "react"
+
+// import LoginForm from "@/components/layout/LoginForm"
+
+// export default function LoginPage() {
+//   return (
+//     <section className="contents__wrap login">
+//       <LoginForm />
+//     </section>
+//   )
+// }
