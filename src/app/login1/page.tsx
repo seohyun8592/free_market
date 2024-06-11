1faa532dff2935668f700732c101ada973703ae8
lanie <lanie.seo@kakaocorp.com>
2024-06-11 15:22:47 +0900

    test888888

--- src/app/login1/page.tsx
+++ src/app/login1/page.tsx
@@ -6,8 +6,8 @@ import LoginForm from "@/components/layout/LoginForm"
 
 export default function Login() {
   return (
-    <>
+    <section className="contents__wrap">
       <LoginForm />
-    </>
+    </section>
   )
 }
