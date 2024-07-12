82a0c0e38b750023086ac90b1f218b3c21283565
lanie <lanie.seo@kakaocorp.com>
2024-07-12 12:24:02 +0900

    test5

--- src/components/layout/JoinForm.tsx
+++ src/components/layout/JoinForm.tsx
@@ -239,13 +239,12 @@ export default function JoinForm() {
     if (popup && !popup.closed) {
       console.log("자식창이 열려 있습니다.")
       // setGetCookie(getCookieFromString("email", document.cookie))
-      setGetCookie(document.cookie)
-      console.log(getCookie)
-      if (getCookie) {
-        setTimeout(() => {
-          popup.close()
-        }, 1000)
-      }
+      setTimeout(() => {
+        setGetCookie(document.cookie)
+        console.log(getCookie)
+
+        popup.close()
+      }, 1000)
     }
   }
 
