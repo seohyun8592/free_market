2c906a393785237b301e89ec8528d57076f71aee
lanie <lanie.seo@kakaocorp.com>
2024-07-22 16:08:39 +0900

    test9

--- src/api/kakaoLogin.ts
+++ src/api/kakaoLogin.ts
@@ -1,24 +1,23 @@
-// import Oauth2PageProps from '../../app/login/oauth2/code'
-// import { Oauth2PageProps } from "@/app/login/oauth2/code/[slug]/page"
+// // import Oauth2PageProps from '../../app/login/oauth2/code'
+// // import { Oauth2PageProps } from "@/app/login/oauth2/code/[slug]/page"
 
-interface SocialParams {
-  slug: string
-  code: string | null
-}
+// interface SocialParams {
+//   slug: string
+//   code: string | null
+// }
 
-const kakaoLogin = ({ code, slug }: SocialParams) => {
-  fetch(`https//front.devsj.site/login/oauth2/code/${slug}?code=${code}`)
-    .then((res) => {
-      console.log("res", res) // 토큰이 넘어올 것임
-      return res.json()
-    })
-    .then((data) => {
-      console.log("data", data) // 응답 데이터를 출력
-      const SOCIAL_EMAIL = document.cookie
-      localStorage.setItem("email", SOCIAL_EMAIL)
-    })
-    .catch((err) => {
-      console.log("소셜로그인 에러", err)
-    })
-}
-export default kakaoLogin
+// const kakaoLogin = ({ code, slug }: SocialParams) => {
+//   fetch(`https//front.devsj.site/login/oauth2/code/${slug}?code=${code}`)
+//     .then((res) => {
+//       console.log("res", res) // 토큰이 넘어올 것임
+//       return res.json()
+//     })
+//     .then((data) => {
+//       const SOCIAL_EMAIL = document.cookie
+//       localStorage.setItem("email", SOCIAL_EMAIL)
+//     })
+//     .catch((err) => {
+//       console.log("소셜로그인 에러", err)
+//     })
+// }
+// export default kakaoLogin
